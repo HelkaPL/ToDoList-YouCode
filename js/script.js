df897a192a8f6e3981f892ab48e4983baf2b235d
Jakub Helizanowicz <jakubhelizanowicz@gmail.com>
2023-01-26 14:01:47 +0100

    refactoring tasks list with immutability

--- js/script.js
+++ js/script.js
@@ -1,82 +1,100 @@
-// Array below include sample tasks
-const tasks = [
-	{
-		content: 'Pierwsze zadanie, wciaz niewykonane',
-		done: false,
-	},
-	{
-		content: 'Drugie zadanie, już wykonane',
-		done: true,
-	},
-]
-// End of sample tasks array
-const taskContent = document.querySelector('.taskForm__taskContent')
+{
+    // Array below include sample tasks
+    let tasks = [
+        {
+            content: 'Pierwsze zadanie, wciaz niewykonane',
+            done: false,
+        },
+        {
+            content: 'Drugie zadanie, już wykonane',
+            done: true,
+        },
+    ]
+    // End of sample tasks array
+    const taskContent = document.querySelector('.taskForm__taskContent')
 
-const addNewTask = newTaskContent => {
-	tasks.push({
-		content: newTaskContent,
-	})
-	render()
-}
+    const addNewTask = (newTaskContent) => {
+        tasks = [
+            ...tasks,
+            { content: newTaskContent },
+        ];
+        render();
+    }
 
-const removeTask = index => {
-    tasks.splice(index, 1)
-    render()
-}
+    const removeTask = index => {
+        tasks = [
+            ...tasks.slice(0, index),
+            ...tasks.slice(index + 1),
+        ];
+        render()
+    }
 
-const toggleTask = index => {
-    tasks[index].done = !tasks[index].done
-    render()
-}
+    const toggleTask = index => {
+        tasks = [
+            ...tasks.slice(0, index),
+            { ...tasks[index], done: !tasks[index].done },
+            ...tasks.slice(index + 1),
+        ];
+        render()
+    }
 
-const onFormSubmit = () => {
-    const newTaskContent = taskContent.value.trim()
+    const onFormSubmit = () => {
+        const newTaskContent = taskContent.value.trim()
         if (newTaskContent === '') {
             return
         }
         addNewTask(newTaskContent)
     }
-const bindEvents = () => {
-    const removeButtons = document.querySelectorAll('.js-removeTask')
-    removeButtons.forEach((removeButton, index) => {
-        removeButton.addEventListener("click", () => {
-            removeTask(index);
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll('.js-removeTask')
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            })
         })
-    })
 
-    const toggleTaskButtons = document.querySelectorAll('.js-toggleTask')
-    toggleTaskButtons.forEach((toggleTaskButton, index) => {
-        toggleTaskButton.addEventListener("click", () => {
-            toggleTask(index);
+        const toggleTaskButtons = document.querySelectorAll('.js-toggleTask')
+        toggleTaskButtons.forEach((toggleTaskButton, index) => {
+            toggleTaskButton.addEventListener("click", () => {
+                toggleTask(index);
+            })
         })
-    })
-}
+    }
 
-const render = () => {
-	let htmlString = ''
-	for (const task of tasks) {
-		htmlString += `
+    const render = () => {
+        let htmlString = ''
+        for (const task of tasks) {
+            htmlString += `
         <li class="taskList${task.done ? ' taskList--done' : ''}">
-        <input type="button" class="taskList__button taskList__button--add js-toggleTask" value="&#10004;">
+        <input
+          type="button" 
+          class="taskList__button taskList__button--add js-toggleTask"
+          value="&#10004;"
+        >
         <span class="taskList__taskText">${task.content}</span>
-        <input type="button" class="taskList__button taskList__button--remove js-removeTask" value="&#128465;">
+        <input
+          type="button"
+          class="taskList__button taskList__button--remove js-removeTask"
+          value="&#128465;"
+        >
         </li>
         `
-	}
-	document.querySelector('.js-tasks').innerHTML = htmlString
-    bindEvents()    
+        }
+        document.querySelector('.js-tasks').innerHTML = htmlString
+        bindEvents()
+    }
+
+    const init = () => {
+        render()
+
+        const form = document.querySelector('.js-form')
+        form.addEventListener('submit', e => {
+            e.preventDefault();
+            onFormSubmit();
+            taskContent.value = '';
+            taskContent.focus();
+        })
+    }
+
+    init()
 }
-
-const init = () => {
-    render()
-
-    const form = document.querySelector('.js-form')
-	form.addEventListener('submit', e => {
-		e.preventDefault()
-		onFormSubmit()
-		taskContent.value = ''
-		taskContent.focus()
-	})
-}
-
-init()
